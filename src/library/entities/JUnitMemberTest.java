0a72c2872cc59e84657d6404e16cb7f839a640c9
Marcel Wiles <deadscar34@gmail.com>
2015-09-29 17:25:16 +1000

    Moved the entities and test to the appropriate location and added DAO

--- src/library/entities/JUnitMemberTest.java
+++ src/library/entities/JUnitMemberTest.java
@@ -1,4 +1,4 @@
-package library;
+package library.entities;
 
 import static org.junit.Assert.*;
 
--- src/library/entities/Member.java
+++ src/library/entities/Member.java
@@ -1,4 +1,4 @@
-package library;
+package library.entities;
 
 import java.util.ArrayList;
 import java.util.List;
