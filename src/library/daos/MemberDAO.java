79904e1ec458dfba31de6461c2d8f11e5d46ddd7
Marcel Wiles <deadscar34@gmail.com>
2015-09-29 19:21:59 +1000

    Created DAO and helper + their JUnit Tests

--- src/library/daos/MemberDAO.java
+++ src/library/daos/MemberDAO.java
@@ -1,46 +1,76 @@
 package library.daos;
 
+import java.util.ArrayList;
 import java.util.List;
 
 import library.interfaces.daos.IMemberDAO;
 import library.interfaces.entities.IMember;
 
 public class MemberDAO implements IMemberDAO{
-
+	
+	MemberHelper helper;
+	List<IMember> members = new ArrayList<IMember>();
+	
+	public MemberDAO(MemberHelper helper){
+		this.helper = helper;
+	}
+	
 	@Override
-	public IMember addMember(String firstName, String lastName, String ContactPhone, String emailAddress) {
-		// TODO Auto-generated method stub
-		return null;
+	public IMember addMember(String firstName, String lastName, String contactPhone, String emailAddress) {
+		IMember newMember = helper.makeMember(firstName, lastName, contactPhone, emailAddress, members.size() + 1);
+		members.add(newMember);
+		return newMember;
 	}
 
 	@Override
 	public IMember getMemberByID(int id) {
-		// TODO Auto-generated method stub
+		for(int i = 0; i < members.size(); i++){
+			if(members.get(i).getID() == id){
+				return members.get(i);
+			}
+		}
 		return null;
 	}
 
 	@Override
 	public List<IMember> listMembers() {
-		// TODO Auto-generated method stub
-		return null;
+		return members;
 	}
 
 	@Override
 	public List<IMember> findMembersByLastName(String lastName) {
-		// TODO Auto-generated method stub
-		return null;
+		List<IMember> membersFound = new ArrayList<IMember>();
+
+		for(int i = 0; i < members.size(); i++){
+			if(members.get(i).getLastName() == lastName){
+				membersFound.add(members.get(i));
+			}
+		}
+		return membersFound;
 	}
 
 	@Override
 	public List<IMember> findMembersByEmailAddress(String emailAddress) {
-		// TODO Auto-generated method stub
-		return null;
+		List<IMember> membersFound = new ArrayList<IMember>();
+
+		for(int i = 0; i < members.size(); i++){
+			if(members.get(i).getEmailAddress() == emailAddress){
+				membersFound.add(members.get(i));
+			}
+		}
+		return membersFound;
 	}
 
 	@Override
 	public List<IMember> findMembersByNames(String firstName, String lastName) {
-		// TODO Auto-generated method stub
-		return null;
+		List<IMember> membersFound = new ArrayList<IMember>();
+
+		for(int i = 0; i < members.size(); i++){
+			if(members.get(i).getFirstName() == firstName && members.get(i).getLastName() == lastName){
+				membersFound.add(members.get(i));
+			}
+		}
+		return membersFound;
 	}
 
 }
